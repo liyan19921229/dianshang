fe3601ef1fe74252c72dd04cdd9b710a00fdad74
liyan <424412729@qq.com>
2018-05-03 14:58:11 +0800

    fenlei

--- backend/views/login/adminlogin.php
+++ backend/views/login/adminlogin.php
@@ -1,9 +1,5 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd
-
-">
-<html xmlns="http://www.w3.org/1999/xhtml
-
-">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>后台管理系统</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -12,7 +8,7 @@
 <style>
 body{width:100%;height:100%;overflow:hidden;background:url(/public/images/pc_loginBg.jpg) no-repeat;background-size:cover;position:absolute;}
 </style>
-<script src="/public/js/jquery.js"></script>
+<script src="/public/js/jquery.js"></script>+
 <script src="/public/js/Particleground.js"></script>
 <script>
 // $(document).ready(function() {
@@ -24,9 +20,9 @@ body{width:100%;height:100%;overflow:hidden;background:url(/public/images/pc_log
 //     'margin-top': -($('.intro').height() /2)
 //   });
 //   $(".loginform input[type='button']").click(function(){
-//    alert("登陆成功，程序对接时将其删除，此处测试！");
-//    location.href="/index/index";
-//    });
+// 	  alert("登陆成功，程序对接时将其删除，此处测试！");
+// 	  location.href="/index/index";
+// 	  });
 // });
 </script>
 
